ddb5705278d4d686a537aef3163f8cddc5a8c61a
anukih749-collab <anukih749@gmail.com>
2026-09-19 12:27:53 +0700

    Biodata.java

---
+++ Biodata.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama     :moch.koirul amri ");
+        System.out.println("Nim :261301006 ");
+        System.out.println("Jurusan  : Sistem Komputer");
+        System.out.println("Universitas: Universitas Nahdlatul Ulama");
+    }
+}
